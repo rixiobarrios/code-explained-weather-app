062bf9ec3bd995e7689406fc80525d65b205d3b9
Rixio Barrios <55994508+rixiobarrios@users.noreply.github.com>
2020-07-12 09:31:49 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,94 @@
+// API key: 82005d27a116c2880c8f0fcb866998a0
+// Alternate API key : 0ebf0e29926cc939f557a936228e1129
+
+// Select Elements
+const iconElement = document.querySelector('.weather-icon');
+const tempElement = document.querySelector('.temperature-value p');
+const descElement = document.querySelector('.temperature-description p');
+const locationElement = document.querySelector('.location p');
+const notificationElement = document.querySelector('.notification');
+
+// App data
+const weather = {};
+
+weather.temperature = {
+    unit: 'celsius',
+};
+
+// App Variables
+const Kelvin = 273;
+// API Key
+const key = '0ebf0e29926cc939f557a936228e1129';
+
+// Geolocation Browser Request
+if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+} else {
+    notificationElement.style.display = 'block';
+    notificationElement.innerHTML =
+        "<p>Browser doesn't Support Geolocation</p>";
+}
+
+// User's Position
+function setPosition(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    getWeather(latitude, longitude);
+}
+
+// Show Display Error
+function showError(error) {
+    notificationElement.style.display = 'block';
+    notificationElement.innerHTML = `<p> ${error.message} </p>`;
+}
+
+// Get weather from API
+function getWeather(latitude, longitude) {
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+
+    fetch(api)
+        .then(function (response) {
+            let data = response.json();
+            return data;
+        })
+        .then(function (data) {
+            weather.temperature.value = Math.floor(data.main.temp - Kelvin);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function () {
+            displayWeather();
+        });
+}
+
+// Display Weather to UI
+function displayWeather() {
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
+
+// C to F Conversion
+function celsiusToFahrenheit(temperature) {
+    return (temperature * 9) / 5 + 32;
+}
+
+// Response from User Clicking on Temperature to Convert it
+tempElement.addEventListener('click', function () {
+    if (weather.temperature.value === undefined) return;
+
+    if (weather.temperature.unit == 'celsius') {
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+
+        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
+        weather.temperature.unit = 'fahrenheit';
+    } else {
+        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+        weather.temperature.unit = 'celsius';
+    }
+});
