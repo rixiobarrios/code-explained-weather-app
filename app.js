ecb057f91a44ef9b9c742727fb568a925b500d78
rixiobarrios <rixiobarrios.consulado@gmail.com>
2020-07-14 12:58:26 -0500

    made fetch link secured

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ function showError(error) {
 
 // Get weather from API
 function getWeather(latitude, longitude) {
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
 
     fetch(api)
         .then(function (response) {
